726b3cad259822ca2528bafa0b8a3304d01528d7
BWartchow <b.s.wartchow@gmail.com>
2023-05-04 19:32:39 -0300

    material aulas java

---
+++ exemplos_Java/src/main/java/br/com/targettrust/exemplos/dia09/ordenacao/ExercicioOrdenacao.java
@@ -0,0 +1,33 @@
+package br.com.targettrust.exemplos.dia09.ordenacao;
+
+/**
+ - Crie uma classe `Produto` com os atributos: `nome`, `custoAquisicao`, `valorVenda`.
+
+ - Crie uma classe `ExercicioOrdenacao` com o método `main`.
+
+ - Crie os produtos na lista.
+
+ - Imprima a lista:
+
+ + Ordenada por `nome`.
+ + Ordenada por `custoAquisicao`.
+
+ Produtos:
+
+ Nome, CustoAquisicao, ValorVenda
+
+ Creme Dental 90g, R$ 2,49, R$ 2,99
+ Sabonete em Barra Corporal 90g, R$ 2,99, R$ 3,30
+ Protetor Solar 30 FPS 200ml, R$ 37,39, R$ 39,12
+ Fralda P Confort - 50 Unidades, R$ 44,90, R$ 44,90
+ Condicionador 200ml, R$ 18,90, R$ 19,50
+
+ */
+public class ExercicioOrdenacao {
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
